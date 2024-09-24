dbec2fe86fe9164af812f986e8f2cbb4d47d0df2
MarlonHenrique7 <marlonhenrique777@gmail.com>
2024-09-24 14:29:09 -0300

    feat: user service layer

---
+++ src/main/java/com/springproject/spring/services/UserService.java
@@ -0,0 +1,27 @@
+package com.springproject.spring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springproject.spring.entities.User;
+import com.springproject.spring.repositories.copy.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
